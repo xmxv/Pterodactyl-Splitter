8a7268fda8601c911ad6b64e1bc018cfb0264f50
Leon <71974419+xmxv@users.noreply.github.com>
2026-05-18 11:17:54 +0300

    1

--- splitter/components/api/deleteSplitServer.ts
+++
@@ -1,5 +0,0 @@
-import http from '@/api/http';
-
-export const deleteSplitServer = async (serverUuid: string, splitUuid: string): Promise<void> => {
-    await http.delete(`/api/client/extensions/{identifier}/servers/${serverUuid}/split/${splitUuid}`);
-};
